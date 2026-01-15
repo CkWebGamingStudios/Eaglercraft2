2f81fcdcfd364ebc9190cd8c0e426f484bdf59a0
CkWebGamingStudios <ckwebgaming@gmail.com>
2026-01-15 17:51:40 +0530

    Google Web Console

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import Sitemap from 'vite-plugin-sitemap'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react({
       babel: {
         plugins: [['babel-plugin-react-compiler']],
       },
+    }),
+    Sitemap({
+      hostname: 'https://eaglercraft2ck.pages.dev',
+      // Add all your subpages here so Google knows they exist
+      dynamicRoutes: [
+        '/editor',
+        // '/about', 
+        // '/contact'
+      ],
     }),
   ],
 })
