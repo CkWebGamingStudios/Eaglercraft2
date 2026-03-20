37d3f30fb818d40e5a0e1f8366af77c513e27610
ckwebgaming <ckwebgaming@gmail.com>
2026-03-20 10:02:44 +0000

    eaglercraft2

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
-
 import express from 'express';
-import bodyParser from 'body-parser';
 
 const app = express();
 const port = 3001;
 
-app.use(bodyParser.json());
+app.use(express.json());
 
 let messages = [];
 
