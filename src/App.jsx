2c5ac7880ec4d09be5d5c40b24f02834155d2a4d
Ck Web Gaming Studios <ckwebgaming@gmail.com>
2026-03-23 13:10:40 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import Login from "./pages/Login.jsx";
-import Forums from "./pages/Forums.jsx";
+import Forums from "./pages/ForumsV2.jsx";
 import Navbar from "./Navbar.jsx"; // Import the new Navbar component
 import { clearCachedProfile, fetchAuthSessionUser, loadCachedProfile, logoutAuthSession, redirectToProviderLogin, saveCachedProfile } from "./utils/authHeader.js";
 
